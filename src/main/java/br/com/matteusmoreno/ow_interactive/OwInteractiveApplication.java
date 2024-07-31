b825b763c7fe5f3a2fee09e55b048da97a4bc492
Matteus Moreno <matteus_moreno@live.com>
2024-07-31 14:18:29 -0300

    initial commit

---
+++ src/main/java/br/com/matteusmoreno/ow_interactive/OwInteractiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.ow_interactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwInteractiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwInteractiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/matteusmoreno/ow_interactive/OwInteractiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.matteusmoreno.ow_interactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwInteractiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
